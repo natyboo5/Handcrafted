5044ef7c5aed44417edbe922f2b4d3783a7a9b74
diannaqui <qui22001@byui.edu>
2024-05-10 20:33:19 -0600

    categories

--- handcrafted_haven_api/src/app/handcrafted-haven/(overview)/page.tsx
+++ handcrafted_haven_api/src/app/handcrafted-haven/(overview)/page.tsx
@@ -1,3 +1,18 @@
+
+import Categories from "@/app/ui/categories/page";
 export default function Page() {
-  return <h1> Home Page - landing page</h1>;
+  return (
+    <main>
+      <h1> Home Page - landing page</h1>
+
+
+
+
+      {/* Category section */}
+      <h2> Shop by Category</h2>
+      <Categories />
+          
+    </main>
+    
+  );
 }
